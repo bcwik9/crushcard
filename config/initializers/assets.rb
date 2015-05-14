f55b8f46838a57f3ffb9654142a14a0e3c808a1c
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-05-14 01:26:32 +0000

    install bootstrap theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( style.css cards.js )
+Rails.application.config.assets.precompile += %w( style.css cards.js grayscale.js bootstrap_and_overrides.css )
